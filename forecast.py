ae71f962b597fe139fe40db6b715d552e3a1d7e8
scott abernathy <abernse@gmail.com>
2023-05-03 07:18:02 -0500

    successfully getting 5 day forecast object data

--- forecast.py
+++ forecast.py
@@ -1,13 +1,18 @@
 import requests
 import mysecrets
+import json
 
 class forecast_data:
     # api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
     def __init__(self,geo_data):
-        res = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={geo_data.lat}&lon={geo_data.lon}&cnt=5&appid={mysecrets.api_key}")
+        res = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={geo_data.lat}&lon={geo_data.lon}&appid={mysecrets.api_key}")
         forecast_data = res.json()
+        self.forecast = []
+        for w in forecast_data['list']:
+            self.forecast.append(daily_data(w['dt'],w))
 
 
-# class daily_data: 
-#     def __init__(self) -> None:
-#         self.
+class daily_data: 
+    def __init__(self,day,forecast) -> None:
+        self.day = day
+        self.weather = forecast['weather'][0]['main']
